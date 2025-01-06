fa249a3c293980cfaffe0925842fe4fe947ac63a
TheDevCaio <caioinnocencio@gmail.com>
2025-01-06 20:21:11 -0300

    Create Emoji.tsx

---
+++ aula06/src/Emoji.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./Emoji.css"
+
+const EMOJIS = new Map<string, string>([
+["happy", "😊"],
+["sick", "🤢"],
+["dead", "💀"]
+]);
+
+
+export default function Emoji(){
+  
+  const [situacao, setSituacao] = useState("happy");
+
+  function toHappy(){
+    console.log("toHappy()!");
+    setSituacao("happy");
+  }
+
+  function toDead(){
+    console.log("toDead()!");
+    setSituacao("dead");
+  }
+
+  return(
+<div className="emoji">
+    <div className="situacao">{EMOJIS.get(situacao) || "😐"}</div>
+    <div className="acoes">
+        <button onClick={toDead}>Morto</button>
+        <button onClick={toDead}>Vivo</button>
+    </div>
+</div>
+  )
+
+    }
